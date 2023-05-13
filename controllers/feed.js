4ae0b20afce2c52d03f9a8ba6ac8134acd0ef04d
Samuel <samueltwumasi1290.com>
2023-05-13 21:50:47 +0000

    update

---
+++ controllers/feed.js
@@ -0,0 +1,29 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: 1,
+        title: "First post",
+        content: "This is the first post",
+        imageUrl: "/image/pic.png",
+        creator: {
+          name: "Samuel",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res.status(201).json({
+    message: "Post created successfully",
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
+    },
+  });
+};
